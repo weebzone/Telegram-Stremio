ab7ff7d446a211547b8850b6c417e9960187109f
Karan Adhikari <doc.adhikari@gmail.com>
2026-06-30 15:15:25 +0530

    Update __init__.py

--- Backend/__init__.py
+++ Backend/__init__.py
@@ -1,14 +1,13 @@
-from Backend.helper.database import Database
-from time import time
 from datetime import datetime
+from time import time
 import pytz
+from Backend.helper.database import Database
 
 timezone = pytz.timezone("Asia/Kolkata")
 now = datetime.now(timezone)
 StartTime = time()
 
-
 USE_DEFAULT_ID: str = None
 db = Database()  
 
-__version__ = "3.4.0"
+__version__ = "3.5.0"
